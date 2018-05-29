f00dcb5bf29febbd56a0b238059553ce1696e5df
Tadeu Grigolon <degrigo@gmail.com>
2018-05-28 21:46:15 -0300

    developed for example

--- aula06/javascript-master/table_example/js/main.js
+++ aula06/javascript-master/table_example/js/main.js
@@ -11,13 +11,36 @@
 //append adiciona o elemento
 // não precisa fechar o elemento html. ele cria o final da tag sozinho
 
+var data = [
+    ["jQuery","Biblioteca","16:30 PM - 22:30 AM'","Hector Vido","13"],
+    ["Linux","Sistema","12:00 PM - 21:30 AM'","Jonathan Alves","12"],
+    ["Python","Linguagem","14:30 PM - 20:30 AM'","Lucas Beyeler","08"],
+    ["Segurança","Pentest","15:00 PM - 16:30 AM'","Leonardo Mendes","09"],
+    ["Yoga","Meditação","17:30 PM - 10:30 AM'","Paramahansa Yogananda","16"],
+    ["Linux","Boas Práticas","18:30 PM - 09:30 AM'","Julio Balott","33"],
+    ["Linux","Mas Práticas","22:00 PM - 08:30 AM'","Gabriel Policante","05"]
+];
+
+// var definindo que a variável está dentro do for somente.
+// percorrendo arrays
+for(var i = 0; i < data.length; i++){
+    //console.log(data[i]);
+    // percorrendo valores por índices dos arrays
+    for(var t = 0; t < data[i].length; t++){
+        console.log(data[i][t]);
+    }
+}
+
 var css_class = 'cell100 column';
 
-var i1 = $('<i>').addClass('fa fa-plus-circle').click();
-var i2 = $('<i>').addClass('fa fa-ban').click(function(e) {
-    
+var i1 = $('<i>').addClass('fa fa-plus-circle').mouseover(function(e) {
+    alert('ok');
     });
-
+    // e represena o evento de disparo
+var i2 = $('<i>').addClass('fa fa-ban').click(function(e) {
+    alert('ok2');
+    });
+//i1.off('mouseover') remove o evento
 
 $('tbody')
     .append($('<tr>')
@@ -37,7 +60,7 @@ $('tbody')
             .append($('<td>')
                     .addClass(css_class + '5')
                         .append(i1)
-                        .append(' ')
+                        .append(' ') // gambiarra
                         .append(i2)
                    )            
             );
